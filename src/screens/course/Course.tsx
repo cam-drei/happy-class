e3d57783a322b9862f5741c29a713381ba903fd6
Vinh Luu <vinhlt3@gmail.com>
2024-01-25 07:43:47 +0700

    Course page temp

--- src/screens/course/Course.tsx
+++ src/screens/course/Course.tsx
@@ -1,18 +1,26 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View} from 'react-native';
 import {Text} from '@rneui/base';
 import styles from './styles';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import HeaderRight from '../../components/header/HeaderRight';
+import axios from 'axios';
+import {baseUrl} from '../../utils/apiConfig';
 
 interface CourseProps {
   navigation: any;
   route: {params: {userName: string; authToken: string}};
 }
 
+interface Course {
+  id: number;
+  name: string;
+}
+
 function Course({navigation, route}: CourseProps) {
   const {userName, authToken} = route.params;
+  const [enrolledCourses, setEnrolledCourses] = useState<Course[]>([]);
 
   const navigateToLesson = () => {
     navigation.navigate('Lesson', {userName, authToken});
@@ -21,6 +29,29 @@ function Course({navigation, route}: CourseProps) {
   const navigateToEnroll = () => {
     navigation.navigate('Enroll', {userName, authToken});
   };
+
+  useEffect(() => {
+    const fetchEnrolledCourses = async () => {
+      try {
+        const response = await axios.get(
+          `${baseUrl}/api/v1/users/${userId}/enrolled_courses`,
+          {
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
+          },
+        );
+
+        setEnrolledCourses(response.data.enrolled_courses);
+      } catch (error) {
+        console.error('Error fetching enrolled courses:', error);
+      }
+    };
+
+    if (authToken) {
+      fetchEnrolledCourses();
+    }
+  }, [authToken]);
 
   useEffect(() => {
     navigation.setOptions({
@@ -39,7 +70,47 @@ function Course({navigation, route}: CourseProps) {
     <View style={styles.container}>
       {isLoggedIn ? (
         <>
-          <View style={styles.box}>
+          {enrolledCourses.length > 0 ? (
+            enrolledCourses.map(course => (
+              <View key={course.id} style={styles.box}>
+                <View style={styles.titleView}>
+                  <Text h4 style={styles.boxTitle}>
+                    {course.name}
+                  </Text>
+                  <FontAwesome
+                    name="book"
+                    size={30}
+                    color="#4F7942"
+                    onPress={() => navigation.goBack()}
+                  />
+                </View>
+                <View style={[styles.titleView]}>
+                  <View style={styles.contentView}>
+                    <FontAwesome
+                      name="play-circle"
+                      size={60}
+                      color="#FF9900"
+                      style={styles.iconPlay}
+                      onPress={navigateToLesson}
+                    />
+                    <View>
+                      <Text style={styles.progressText}>Progress: 150/170 lessons</Text>
+                      <Text style={styles.statusText}>Status: in progress</Text>
+                    </View>
+                  </View>
+                  <AntDesign
+                    name="edit"
+                    size={30}
+                    color="#4F7942"
+                    onPress={navigateToEnroll}
+                  />
+                </View>
+              </View>
+            ))
+          ) : (
+            <Text h4>No enrolled courses.</Text>
+          )}
+          {/* <View style={styles.box}>
             <View style={styles.titleView}>
               <Text h4 style={styles.boxTitle}>
                 ABEKA K4
@@ -107,7 +178,7 @@ function Course({navigation, route}: CourseProps) {
                 onPress={navigateToEnroll}
               />
             </View>
-          </View>
+          </View> */}
         </>
       ) : (
         <View>
