763fd36c03dab4ff432f1e43b32cd74ae4daf30a
Vinh Luu <vinhlt3@gmail.com>
2024-11-01 15:20:38 +0700

    revert baseUrl

--- src/utils/apiConfig.ts
+++ src/utils/apiConfig.ts
@@ -1,2 +1,2 @@
-// export const baseUrl: string = 'http://localhost:3000/api/v1'; //for ios, 'rails server'
-export const baseUrl: string = 'http://10.0.2.2:3000/api/v1'; //for android, 'rails server -b 0.0.0.0'
+export const baseUrl: string = 'http://localhost:3000/api/v1'; //for ios, 'rails server'
+// export const baseUrl: string = 'http://10.0.2.2:3000/api/v1'; //for android, 'rails server -b 0.0.0.0'
